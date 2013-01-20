2ece3015cc1696d3853bcefed566e050391078c8
Desmond Foo <desmond.tl.foo@gmail.com>
2013-01-19 20:11:42 -0500

    Finished Part I of prelab.

---
+++ lab1/part1.v
@@ -0,0 +1,7 @@
+// Simple module that connects the SW switches to the LEDR lights
+module part1(SW, LEDR);
+  input [17:0]SW;    // toggle switches
+  output [17:0]LEDR; //red LEDs
+  
+  assign LEDR = SW;
+endmodule
